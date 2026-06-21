08943805d6104f263248ad8ea70dc3b352f9c336
Gustavo Nobrega <gustavofn@gmail.com>
2026-06-21 16:42:44 -0700

    Sync build pipeline to JDK 21 and gfnord image name

--- publish.sh
+++ publish.sh
@@ -3,12 +3,12 @@
 docker-build(){
     local from_tag=${1}
     local to_tag=${2}
-    echo "docker build --rm --force-rm -t odavid/my-bloody-jenkins:${to_tag} --build-arg=FROM_TAG=${from_tag} ."
-    docker build --rm --pull --force-rm -t odavid/my-bloody-jenkins:${to_tag} --build-arg=FROM_TAG=${from_tag} .
-    docker tag odavid/my-bloody-jenkins:${to_tag} ghcr.io/odavid/my-bloody-jenkins:${to_tag}
-    echo "docker push odavid/my-bloody-jenkins:${to_tag}"
-    docker push odavid/my-bloody-jenkins:${to_tag}
-    docker push ghcr.io/odavid/my-bloody-jenkins:${to_tag}
+    echo "docker build --rm --force-rm -t gfnord/my-bloody-jenkins:${to_tag} --build-arg=FROM_TAG=${from_tag} ."
+    docker build --rm --pull --force-rm -t gfnord/my-bloody-jenkins:${to_tag} --build-arg=FROM_TAG=${from_tag} .
+    docker tag gfnord/my-bloody-jenkins:${to_tag} ghcr.io/gfnord/my-bloody-jenkins:${to_tag}
+    echo "docker push gfnord/my-bloody-jenkins:${to_tag}"
+    docker push gfnord/my-bloody-jenkins:${to_tag}
+    docker push ghcr.io/gfnord/my-bloody-jenkins:${to_tag}
 }
 
 lts_version=$(cat LTS_VERSION.txt)
@@ -18,7 +18,7 @@ latest)
     docker-build ${lts_version}-alpine latest
     docker-build ${lts_version}-alpine alpine
     docker-build ${lts_version} debian
-    docker-build ${lts_version}-jdk11 jdk11
+    docker-build ${lts_version}-jdk21 jdk21
     ;;
 v*)
     tag=$(echo $version_type | sed 's/v//g')
@@ -33,16 +33,16 @@ v*)
     docker-build ${lts_version} ${short_tag}-debian
     docker-build ${lts_version} lts-debian
 
-    docker-build ${lts_version}-jdk11 ${tag}-jdk11
-    docker-build ${lts_version}-jdk11 ${short_tag}-jdk11
-    docker-build ${lts_version}-jdk11 lts-jdk11
+    docker-build ${lts_version}-jdk21 ${tag}-jdk21
+    docker-build ${lts_version}-jdk21 ${short_tag}-jdk21
+    docker-build ${lts_version}-jdk21 lts-jdk21
     ;;
 *)
     tag=$version_type
     docker-build ${lts_version}-alpine $tag
     docker-build ${lts_version}-alpine $tag-alpine
     docker-build ${lts_version} $tag-debian
-    docker-build ${lts_version}-jdk11 $tag-jdk11
+    docker-build ${lts_version}-jdk21 $tag-jdk21
     ;;
 esac
 #docker-build $1 $2
